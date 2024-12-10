3ad0af33f724ef7e5766d98ddd2d6bbd42ee07da
jumerckx <julesmerckx12@gmail.com>
2024-12-10 15:57:39 +0100

    wip

--- src/MLIRGen.jl
+++ src/MLIRGen.jl
@@ -36,11 +36,12 @@ isoperand(arg) = "TypeConstraint" ∈ arg.var"!superclasses" || "Arg" ∈ arg.va
 isattribute(arg) = "AttrConstraint" ∈ arg.var"!superclasses"
 isvariadic(arg) = "Variadic" ∈ arg.var"!superclasses"
 isoptional(arg) = "Optional" ∈ arg.var"!superclasses"
+abstract type Def end
 for f in (:isvariadic, :isoperand, :isattribute, :isoptional)
     @eval $f(ctx::Ctx, arg::String) = $f(ctx.json[arg])
+    @eval $f(ctx::Ctx, def::Def) = $f(ctx.json[def.def])
 end
 
-abstract type Def end
 Def(name, def) = isnothing(name) ? UnnamedDef(def) : NamedDef(name, def)
 struct UnnamedDef <: Def
     def::String
@@ -111,6 +112,24 @@ struct Value end
 struct MType end
 struct Region end
 struct Attribute end
+struct Block end
+
+argname(def::Def) = Symbol.(def isa UnnamedDef ? "?" : def.name)
+
+function argtype(ctx::Ctx, basetype, def::Def)
+    T = basetype
+    if isvariadic(ctx, def)
+        T = Vector{T}
+    end
+    if isoptional(ctx, def)
+        T = Union{T, Nothing}
+    end
+    return T
+end
+
+splatted_arg(ctx::Ctx, name::Symbol, def::Def) = isvariadic(ctx, def) ? Expr(:(...), name) : name
+
+optional_push(ctx::Ctx, target, name::Symbol) = :(!isnothing($name) && push!($target, $name))
 
 function arg_exprs(ctx, defs::Vector{Def}, basetype)
     arglist = []
@@ -123,10 +142,10 @@ function arg_exprs(ctx, defs::Vector{Def}, basetype)
         end
 
         T = basetype
-        if isvariadic(ctx, def.def)
+        if isvariadic(ctx, def)
             T = Vector{T}
         end
-        if isoptional(ctx, def.def)
+        if isoptional(ctx, def)
             T = Union{T, Nothing}
         end
 
@@ -139,31 +158,59 @@ end
 typed_list(names, types) = Expr.(:(::), names, types)
 
 function generate_op(ctx::Ctx, op::Operation)
+    # TODO: 
+    # * [ ] optional operands should default to nothing
+    # * [ ] result inference...
+    
     name = Symbol(op.name)
 
-    operandlist, operandtypes = arg_exprs(ctx, op.operands, Value)
-    resultlist, resulttypes = arg_exprs(ctx, op.results, MType)
-    regionlist, regiontypes = arg_exprs(ctx, op.regions, Region)
-    attrlist, attrtypes = arg_exprs(ctx, op.attributes, Attribute)
+    operandnames = argname.(op.operands)
+    operandtypes = argtype.(Ref(ctx), Ref(Value), op.operands)
+
+    resultnames = argname.(op.results)
+    resulttypes = argtype.(Ref(ctx), Ref(MType), op.results)
+
+    regionnames = argname.(op.regions)
+    regiontypes = argtype.(Ref(ctx), Ref(Region), op.regions)
+
+    attrnames = argname.(op.attributes)
+    attrtypes = argtype.(Ref(ctx), Ref(Attribute), op.attributes)
+
+    successornames = argname.(op.successors)
+    successortypes = argtype.(Ref(ctx), Ref(Block), op.successors)
 
     location = gensym(:location)
-
+    
     operands = gensym(:operands)
     results = gensym(:results)
     regions = gensym(:regions)
     attributes = gensym(:attributes)
     successors = gensym(:successors)
 
+    push_stmts = []
+    for (target, defs) in ((operands, op.operands), (results, op.results), (regions, op.regions), (attributes, op.attributes), (successors, op.attributes))
+        for def in defs
+            if isoptional(ctx, def)
+                push!(push_stmts, optional_push(ctx, target, argname(def)))
+            end
+        end
+    end
+
     return quote
         function $name(
-                    $(typed_list(operandlist, operandtypes)...);
-                    $(typed_list(resultlist, resulttypes)...),
-                    $(typed_list(attrlist, attrtypes)...)
+                    $(typed_list(operandnames, operandtypes)...);
+                    $(typed_list(resultnames, resulttypes)...),
+                    $(typed_list(attrnames, attrtypes)...),
+                    $(typed_list(regionnames, regiontypes)...),
                  )
 
-            $(operands) = $(Value)[$(operandlist...)]
-            $(results) = $(MType)[$(resultlist...)]
-            $(regions) = $(Region)[$(regionlist...)]
+            $(operands) = $(Value)[$(operandnames...)]
+            $(results) = $(MType)[$(resultnames...)]
+            $(regions) = $(Region)[$(regionnames...)]
+            $(attributes) = $(Attribute)[$(attrnames...)]
+            $(successors) = $(Block)[$(successornames...)]
+
+            $(push_stmts...)
 
             return $(create_op_placeholder)(
                 $("$(dialectname(ctx, op)).$(op.name)"),
